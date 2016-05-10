88ab2ad6f9f8726bb4e07532b093e5a4f594b143
scofeez <dubois.matthieu@protonmail.com>
2016-05-10 09:59:11 +0200

    exercice 1 terminé

---
+++ exo_1.php
@@ -0,0 +1,10 @@
+<?php
+$pasteques = 80;
+$oranges = 122;
+$legumes = $pasteques + $oranges;
+echo "Le stock de fruits de la réserve est de $legumes".'<br/>';
+$poneys = 12;
+$licornes = 10;
+$norris = $poneys * $licornes;
+echo "Si j'appelle Chuck Norris il te tue $norris fois!";
+?>
